b7ad3cfed7e718b24bd7d1bb9e36bfda80de3c99
Yash Gaitonde <gaitonde@umich.edu>
2020-06-07 17:46:07 +0000

    final version working

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import random
 
-nodeList = []
-edgeList = []
-with open("test.txt") as f:
+# dictionary
+nodeList = {}
+with open("kargerMinCut.txt") as f:
   for line in f:
     temp = []
     temp.append(int(line.split()[0]))
     temp.append([int(n) for n in line.split()[1:]])
-    nodeList.append(temp)
+    nodeList[temp[0]] = temp[1]
 
 def getEdge(): 
-  i = random.choice(nodeList)
-  j = random.choice(nodeList[nodeList.index(i)][1])
-  return [i[0],j]
+  i = random.choice(list(nodeList))
+  j = random.choice(nodeList[i])
+  return [i,j]
 
-# needs to remove association with removed node in all nodes
 def contract(n1,n2): 
-  temp1 = nodeList[n1][1]
-  temp2 = nodeList[n2][1]
-  for i in temp2:
-    if(i!=nodeList[n1][0]):
-        if(i not in temp1):
-            nodeList[n1][1].append(i)
+  for i in nodeList[n2]:
+    if(i!=n1):
+        nodeList[n1].append(i)
   nodeList.pop(n2)
- 
+  for key,value in nodeList.items():
+    value[:] = [n1 if x==n2 else x for x in value]
+    value[:] = [x for x in value if x!=key]
+
 def karger():
   while (len(nodeList) > 2):
     edge = getEdge()
     contract(edge[0],edge[1])
-  return len(nodeList[0][1])
+  val = len(list(nodeList.values())[0])
+  return val
 
 
